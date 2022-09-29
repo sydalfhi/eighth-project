87293578e82ed3895aacecbb336b1469446ca1a6
Syaid Alfarishi <syaidalfarishi@gmail.com>
2022-09-29 10:12:24 +0700

    ganti
    mengganti denga todolist baru

--- script.js
+++ script.js
@@ -1,89 +1,102 @@
-const inputText = document.querySelector('.nama')
-const btnKirim = document.querySelector('#tambah')
-const ul = document.querySelector('ul');
-const konten = document.querySelector('.container');
-const spanHapus = document.querySelector('.hapus');
-const cardH = document.querySelector('.card-header');
-const mode = document.querySelector('.mode');
+const judul = document.querySelector('.judul');
+const isi = document.querySelector('.isi');
+const buat = document.querySelector('button.buat');
 
-// 
-// 
-// 
-function hurufPertamaKapital(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
+let judulNote, isiNote;
+
+
+
+
+
+
+function menampilkanHasilAkhir() {
+    let kotak = document.createElement('div');
+    kotak.classList.add('kotak');
+    kotak.innerHTML = [`
+    <div class="kiri">
+     <h1 class="judulText">${judulNote}</h1>
+    <p class="isiText">${isiNote}</p>
+    </div>
+    <div class="kanan">
+    <button class="edit">edit</button>
+    <button class="remove">hapus</button>
+    </div>
+    `].join('');
+    document.body.appendChild(kotak);
 }
 
+function getNamaJudul() {
+        judulNote = judul.value;
+}
+function getIsi() {
+        isiNote = isi.value;
+}
+function reset() {
+    judul.value = '';
+   isi.value = '';
+    
+}
 
-// 
-// 
-// 
-function satu() {
-    //    
-    if (inputText.value == '') {
-        alert('Harap mengisi bidang inputan');
-    }else{  
-        btnKirim.classList.replace('tambah', 'gantiSetelahDiClick');
-        cardH.classList.add('ganticard');
-        const d = new Date();
-        let tahun = d.getFullYear();
-        let bulan = d.getMonth();
-        let tanggal = d.getDate();
-        let jam = d.getHours();
-        let menit = d.getMinutes();
-        let detik = d.getSeconds();
-        jam = jam < 10 ? `0${d.getHours()}` : `${d.getHours()}`;
-        menit = menit < 10 ? `0${d.getMinutes()}` : `${d.getMinutes()}`;
-        detik = detik < 10 ? `0${d.getSeconds()}` : `${d.getSeconds()}`;            
-        tahun = tahun < 10 ? `0${d.getFullYear()}` : `${d.getFullYear()}`;            
-        bulan = bulan < 10 ? `0${d.getMonth()}` : `${d.getMonth()}`;            
-        tanggal = tanggal  < 10 ? `0${d.getDate()}` : `${d.getDate()}`;            
-
-        let hasilDariUser = inputText.value;
+   
 
 
-        let pesan = {
-                inputan: hurufPertamaKapital(hasilDariUser),
-                tanggal : `${tahun}-${bulan}-${tanggal}      /   ${jam}:${menit}:${detik}`
-        }
+
+
+
+
+
+
+buat.addEventListener('click',()=>{
+    getNamaJudul();
+    getIsi();
+   
+menampilkanHasilAkhir();
+reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.body.addEventListener('click',(e)=>{
+    if (e.target.className == 'edit') {
         
+        judul.value =   e.target.parentElement.parentElement.firstElementChild.firstElementChild.innerHTML;
+        isi.value =   e.target.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML;
+        window.scrollTo(0, 0);
+    }
+    if(e.target.className == 'remove'){
+        e.target.parentElement.parentElement.style.display = 'none';
+            // localStorage.removeItem('name');
+            // e.preventDefault();
+    }
+})
+
+
+
     
 
-        let el = `
-        <li>
-        <h1>${pesan.inputan}</h1>
-        <p>${pesan.tanggal}</p>
-        <span class="hapus">Delete</span>
-        </li>
-        `;
-        
-        
-        ul.innerHTML += el;
-        inputText.value = '';
-        
-        
-        konten.addEventListener('click',(e)=>{
-            if(e.target.innerHTML == 'Delete' ){
-                e.target.parentElement.classList.add('dnone');
-            }
-        });
-
-    }
-}
 
 
-// 
-// 
-// 
-
-mode.addEventListener('click',()=>{
-    ul.classList.toggle('gantiMode');
-});
-
-btnKirim.addEventListener('click',()=>{
-     satu();
-});
 
 
-// 
-
-
+    // hapus.addEventListener('click', (e) => {
+    // });
